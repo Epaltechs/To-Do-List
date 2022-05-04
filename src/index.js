3f23c8c866ce1b1ee64b7f3bb7e27b869d88757d
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-04 21:28:29 +0100

    Add changes to HTML, CSS, and JavaScript Files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './style.css';
+import render from './todosRender';
+import Todos from './todos';
 
-const todosList = [
-  { description: 'Second Task', completed: false, index: 2 },
-  { description: 'First Task', completed: false, index: 1 },
-];
+const todosList = new Todos();
+render(todosList);
 
-const render = (list) => {
-  const sortedTodos = list.sort((a, b) => a.index - b.index);
-  const todosContainer = document.querySelector('.todos');
-  let todosHtml = '';
-  sortedTodos.forEach((todo) => {
-    todosHtml += ` <div class="todo-item">
-        <input type="checkbox" /><span> ${todo.description}</span>
-    </div>`;
-  });
-  todosContainer.innerHTML = todosHtml;
-};
+const addTodoBtn = document.querySelector('.add-btn');
+addTodoBtn.addEventListener('click', () => {
+  const description = document.querySelector('.input-todo').value.trim();
+  const completed = false;
+  const index = todosList.list.length + 1;
+  const newTodo = { description, completed, index };
+  if (description) {
+    todosList.addTodo(newTodo);
+    render(todosList);
+  }
+});
 
 render(todosList);
