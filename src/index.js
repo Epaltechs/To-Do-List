f2d024ff62f5c56df1fef5eb89d7b7600feaef31
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-04 18:24:53 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/*eslint-disable*/
+import './style.css';
+
+const todosList = [
+  { description: 'Second Task', completed: false, index: 2 },
+  { description: 'First Task', completed: false, index: 1 },
+];
+
+const render = (list) => {
+  const sortedTodos = list.sort((a, b) => a.index - b.index);
+  const todosContainer = document.querySelector('.todos');
+  let todosHtml = '';
+  sortedTodos.forEach((todo) => {
+    todosHtml += ` <div class="todo-item">
+        <input type="checkbox" /><span> ${todo.description}</span>
+    </div>`;
+  });
+  todosContainer.innerHTML = todosHtml;
+};
+
+render(todosList);
